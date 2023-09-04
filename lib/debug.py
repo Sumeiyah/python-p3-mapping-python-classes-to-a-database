3fea479c1419a9c82a491e8382f93af5f52dbedf
Farah <sumeyafarah1714@gmail.com>
2023-09-04 17:52:31 +0300

    done

--- lib/debug.py
+++ lib/debug.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/env python
 from config import CONN, CURSOR
 from song import Song
 
 
+import pytest; pytest.set_trace()
+
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+
+Song.create_table()
